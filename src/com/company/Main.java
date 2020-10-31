40f2956a43523917ade3753107f68f55c0c48561
roi <roikreme@post.bgu.ac.il>
2020-10-31 16:17:35 +0200

    menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,33 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        while ((true)){
-            switch (true){
+
+        while (true){
+            System.out.println("1: Add WebUser");
+            System.out.println("2: Remove WebUser ");
+            System.out.println("3: Log In");
+            System.out.println("4: Log Out");
+            System.out.println("5: Make order");
+            System.out.println("6: Display order");
+            System.out.println("7: Add a Link Product");
+            System.out.println("8: Add Product ");
+            System.out.println( "9: Delete Product");
+            System.out.println("10: ShowAllObjects");
+            System.out.println("11: ShowObjectId ");
+            Scanner scanner=new Scanner(System.in);
+            int num=scanner.nextInt();
+            switch (num){
                 case 1:
-                    System.out.println();
+                    System.out.println("Please enter a Login Id");
+                    num=scanner.nextInt();
+
             }
+
         }
 
 
---
+++ src/com/company/WebUser.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class WebUser {
+    String login_id;
+    String password;
+    UseState state;
+}
