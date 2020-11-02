b04fd3b6e874e5727d28a3411183476947808f6f
shahar <gefensha@post.bgu.ac.il>
2020-11-02 18:36:25 +0200

    s

--- src/classes/LineItem.java
+++ src/classes/LineItem.java
@@ -24,6 +24,47 @@ public class LineItem {
         order=null;
         shoppingCart=null;
         product=null;
-
     }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public ShoppingCart getShoppingCart() {
+        return shoppingCart;
+    }
+
+    public void setShoppingCart(ShoppingCart shoppingCart) {
+        this.shoppingCart = shoppingCart;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+
 }
--- src/classes/PremiumAccount.java
+++ src/classes/PremiumAccount.java
@@ -1,4 +1,29 @@
 package classes;
 
+import java.util.HashMap;
+
 public class PremiumAccount extends Account {
+
+    private HashMap<String,Product> hash_Product;
+
+    public PremiumAccount(String id, Customer customer) {
+        super(id, customer);
+        hash_Product = new HashMap<String,Product>();
+    }
+    public void UpdateHashProduct(String id,Product product)
+    {
+        hash_Product.put(id,product);
+    }
+
+    public void DeleteFromHashProduct(String id)
+    {
+        hash_Product.remove(id);
+    }
+
+
+    public void DeletePremium(){
+        for (Product p:hash_Product.values())
+            p.Deletehash();
+        this.Delete();
+    }
 }
