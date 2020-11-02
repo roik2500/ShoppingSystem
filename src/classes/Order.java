092c2a1f3fd4039de0d5a83f369785886a40b789
roypyp <roypyp@gmail.com>
2020-11-02 15:27:49 +0200

    Account, Order and ShoppingCart ver 1

--- src/classes/Order.java
+++ src/classes/Order.java
@@ -1,4 +1,54 @@
 package classes;
 
+import com.sun.xml.internal.ws.wsdl.writer.document.http.Address;
+import enums.OrderStatus;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+
 public class Order {
+    String number;
+    Date ordered;
+    Date shipped;
+    Address ship_to;
+    OrderStatus status;
+    float total;
+    Account account;
+    HashMap<String,Payment> hash_Payment;
+    ArrayList<LineItem> list_LineItem;
+
+    public Order(String number,Account account) {
+        this.number = number;
+        this.account=account;
+        hash_Payment= new HashMap<String, Payment>();
+        list_LineItem=new ArrayList<LineItem>();
+    }
+    public void UpdateHashPayment(String id,Payment payment)
+    {
+        hash_Payment.put(id,payment);
+    }
+
+    public void UpdateListLineItem(LineItem lineItem)
+    {
+        list_LineItem.add(lineItem);
+    }
+    public void DeleteFromHashPayment(String id)
+    {
+        hash_Payment.remove(id);
+    }
+    public void DeleteListLineItem(LineItem lineItem)
+    {
+        list_LineItem.remove(lineItem);
+    }
+    public void Delete(){
+        for (Payment p:hash_Payment.values())
+            p.Delete();
+        hash_Payment=null;
+        for (LineItem l:list_LineItem)
+            l.Delete();
+        list_LineItem=null;
+        account.DeleteFromHashOrder(number);
+
+    }
 }
--- src/classes/ShoppingCart.java
+++ src/classes/ShoppingCart.java
@@ -1,11 +1,35 @@
 package classes;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class ShoppingCart {
     Date created;
+    Account account;
+    WebUser webUser;
+    ArrayList<LineItem> list_LineItem;
 
-    public ShoppingCart(Date created) {
+    public ShoppingCart(Account account,WebUser webUser,Date created) {
+        this.account=account;
+        this.webUser=webUser;
         this.created = created;
+        list_LineItem=new ArrayList<LineItem>();
     }
-
+    public void UpdateListLineItem(LineItem lineItem)
+    {
+        list_LineItem.add(lineItem);
+    }
+    public void DeleteListLineItem(LineItem lineItem)
+    {
+        list_LineItem.remove(lineItem);
+    }
+    public void Delete()
+    {
+        for (LineItem l:list_LineItem)
+            l.Delete();
+        list_LineItem=null;
+        if(webUser!=null)
+            webUser.setShoppingCart(null);
+        account.Delete();
+        account=null;
+    }
 }
