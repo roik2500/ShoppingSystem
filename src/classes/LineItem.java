1c2d1f177c61ee71ec17164958bbf9b38fc25da2
shahar <gefensha@post.bgu.ac.il>
2020-11-02 18:13:51 +0200

    Update LineItem.java

--- src/classes/LineItem.java
+++ src/classes/LineItem.java
@@ -1,4 +1,30 @@
 package classes;
 
+import java.util.Date;
+import java.util.HashMap;
+
 public class LineItem {
+    private int quantity;
+    private int  price;
+    private Order order;
+    private ShoppingCart shoppingCart;
+    private Product product;
+
+    public LineItem(int quantity, int price, Order order, ShoppingCart shoppingCart, Product product) {
+        this.quantity = quantity;
+        this.price = price;
+        this.order = order;
+        this.shoppingCart = shoppingCart;
+        this.product = product;
+    }
+
+    public void Delete(){
+        shoppingCart.DeleteListLineItem(this);
+        product.DeleteListLineItem(this);
+        order.DeleteListLineItems(this);
+        order=null;
+        shoppingCart=null;
+        product=null;
+
+    }
 }
