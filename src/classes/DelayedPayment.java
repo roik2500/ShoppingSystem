51f4395822a7633b1c258aabeff135892a699ac6
Amit-Nadam <Nadama@post.bgu.ac.il>
2020-11-04 00:55:07 +0200

    the black class

--- src/classes/DelayedPayment.java
+++ src/classes/DelayedPayment.java
@@ -1,13 +1,17 @@
 package classes;
-
 import java.util.Date;
 
 public class DelayedPayment extends Payment {
     private Date paymentDate;
 
-    public DelayedPayment(Account account, Order order, Date paymentDate) {
+    public DelayedPayment(Account account, Order order, Date paymentDate,
+                          String id, Date paid, float total, String details) {
         this.account = account;
         this.order = order;
         this.paymentDate = paymentDate;
+        this.id = id;
+        this.paid = paid;
+        this.total = total;
+        this.details = details;
     }
 }
--- src/classes/ImmediatePayment.java
+++ src/classes/ImmediatePayment.java
@@ -1,11 +1,18 @@
 package classes;
+import java.util.Date;
 
 public class ImmediatePayment extends Payment {
     private Boolean phoneConfirmation;
 
-    public ImmediatePayment(Account account, Order order, Boolean phoneConfirmation) {
+
+    public ImmediatePayment(Account account, Order order, Boolean phoneConfirmation,
+                            String id, Date paid, float total, String details) {
         this.account = account;
         this.order = order;
         this.phoneConfirmation = phoneConfirmation;
+        this.id = id;
+        this.paid = paid;
+        this.total = total;
+        this.details = details;
     }
 }
