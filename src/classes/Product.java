6cbcfa247464ae9a34bbd34a1643e578e13c677f
yael6 <yaelkot@post.bgu.ac.il>
2020-11-04 15:12:38 +0200

    Create class Product and Supplier

--- src/classes/Product.java
+++ src/classes/Product.java
@@ -1,4 +1,78 @@
 package classes;
 
+import java.util.ArrayList;
+
 public class Product {
+
+    private String id;
+    private String name;
+    // private String price;  //maybe need to be added
+    private Supplier supplier;
+    private ArrayList<LineItem> listLineItems;
+    private PremiumAccount premiumAccount;
+
+    public Product(String id, String name, Supplier supplier, PremiumAccount premiumAccount) {
+        this.id = id;
+        this.name = name;
+        this.supplier = supplier;
+        this.listLineItems = new ArrayList<LineItem>();
+        this.premiumAccount = premiumAccount;
+    }
+
+    public void UpdateListLineItem(LineItem lineItem) {
+        listLineItems.add(lineItem);
+    }
+
+    public void DeleteListLineItem(LineItem lineItem) {
+        listLineItems.remove(lineItem);
+    }
+
+    public void setPremiumAccount(PremiumAccount premiumAccount) {
+        this.premiumAccount = premiumAccount;
+    }
+
+    public void Delete()
+    {
+        for (LineItem lItem:listLineItems)
+            lItem.Delete();
+        listLineItems = null;
+        this.supplier.DeleteFromHashProduct(id);
+        this.supplier = null;
+        if(this.premiumAccount != null){
+            this.premiumAccount.DeleteFromHashProduct(id);
+            this.premiumAccount = null;
+        }
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Supplier getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(Supplier supplier) {
+        this.supplier = supplier;
+    }
+
+    public ArrayList<LineItem> getListLineItems() {
+        return listLineItems;
+    }
+
+    public void setListLineItems(LineItem lineItem) {
+        {
+            listLineItems.add(lineItem);
+        }
+    }
+
+    public PremiumAccount getPremiumAccount() {
+        return premiumAccount;
+    }
+
+
 }
--- src/classes/Supplier.java
+++ src/classes/Supplier.java
@@ -1,4 +1,43 @@
 package classes;
 
+import java.util.HashMap;
+
 public class Supplier {
+
+    private String id;
+    private String name;
+    private HashMap<String, Product> hashProducts;
+
+    public Supplier(String id, String name) {
+        this.id = id;
+        this.name = name;
+        this.hashProducts = new HashMap<String, Product>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public HashMap<String, Product> getHashProducts() {
+        return hashProducts;
+    }
+
+    public void setHashProducts(String id, Product product) {
+        this.hashProducts.put(id, product);
+    }
+
+    public void DeleteFromHashProduct(String id)
+    {
+        hashProducts.remove(id);
+    }
+
+    public void Delete(){
+       for (Product p:hashProducts.values())
+           p.setPremiumAccount(null);
+       hashProducts.clear();
+    }
 }
