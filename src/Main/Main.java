cfe189cd5947a2f10d20a5f14ced09d397ece3b7
roypyp <roypyp@gmail.com>
2020-11-07 18:37:57 +0200

    roy peled finshed case 1 and 2

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -95,10 +95,23 @@ public class Main {
 
                     //Building the objects:
                     WebUser WebUser = new WebUser(webUesrId, password);//1
+                    AllObjects.put((Integer.toString(counter)),WebUser);
+                    IdListUsers.put(webUser.getLogin_id(),new ArrayList<String>());
+                    IdListUsers.get(webUser.getLogin_id()).add(Integer.toString(counter));
+                    counter++;
                     Customer customer = new Customer(customerId, address, customerPhone, customerEmail,webUser);//2
+                    AllObjects.put((Integer.toString(counter)),customer);
+                    IdListUsers.get(webUser.getLogin_id()).add(Integer.toString(counter));
+                    counter++;
                     if(isPremium == "y") {
                         PremiumAccount premiumAccount = new PremiumAccount(accountId,customer);//3
+                        AllObjects.put((Integer.toString(counter)),premiumAccount);
+                        IdListUsers.get(webUser.getLogin_id()).add(Integer.toString(counter));
+                        counter++;
                         ShoppingCart shoppingCart = new ShoppingCart(premiumAccount,webUser,new Date()); //4
+                        AllObjects.put((Integer.toString(counter)),shoppingCart);
+                        IdListUsers.get(webUser.getLogin_id()).add(Integer.toString(counter));
+                        counter++;
                         webUser.setShoppingCart(shoppingCart);
                         webUser.setCustomer(customer);
                         premiumAccount.setShoppingCart(shoppingCart);
@@ -107,7 +120,13 @@ public class Main {
                     else
                     {
                         Account account1 = new Account(accountId,customer);//3
+                        AllObjects.put((Integer.toString(counter)),account1);
+                        IdListUsers.get(webUser.getLogin_id()).add(Integer.toString(counter));
+                        counter++;
                         ShoppingCart shoppingCart = new ShoppingCart(account1,webUser,new Date()); //4
+                        AllObjects.put((Integer.toString(counter)),shoppingCart);
+                        IdListUsers.get(webUser.getLogin_id()).add(Integer.toString(counter));
+                        counter++;
                         webUser.setShoppingCart(shoppingCart);
                         webUser.setCustomer(customer);
                         account1.setShoppingCart(shoppingCart);
